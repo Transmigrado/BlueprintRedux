75ec9bd088a54600051de4ac0de7620b4e7e8b3f
Jorge Acosta <transmigrado@gmail.com>
2024-05-14 20:04:59 -0400

    feat: first commit

---
+++ BlueprintRedux/BlueprintReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueprintReduxApp.swift
+//  BlueprintRedux
+//
+//  Created by Jorge Acosta Alvarado on 14-05-24.
+//
+
+import SwiftUI
+
+@main
+struct BlueprintReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlueprintRedux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlueprintRedux
+//
+//  Created by Jorge Acosta Alvarado on 14-05-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
